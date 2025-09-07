6b823fbdccb884f4dc6115e8613df64d1d7638cb
Fakhruzaky <d1041211030@student.untan.ac.id>
2025-09-07 18:24:33 +0700

    add nama & nim, change backgroud color

--- App.js
+++ App.js
@@ -5,12 +5,15 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text testID="welcome-text">Hello, React Native!</Text>
+      <Text testID="nama-name">Nama: Muhammad Fakhruzaky Maulyandri</Text>
+      <Text testID="nim-nim">NIM: D1041211030</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#f0f9ff',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
